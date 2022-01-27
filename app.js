4992b219ec42a60a5c8641aa6b760a65505433b0
hari <hariodukkathil@gmail.com>
2022-01-27 11:12:28 +0530

    count of tags in html

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 var fs = require('fs');
-var filename = "data.txt";
+var filename = 'data.html';
 var str = fs.readFileSync(filename).toString();
-var pattern = /man/gim;
+var pattern = /<li>/gim;
+
 var myarray = str.match( pattern );
 var len = myarray.length;
-console.log(`Number of Occurance: ${len}`);
-
-
-// var fs = require('fs');
-// var filename = 'data.txt';
-// var str = fs.readFileSync(filename).toString();
-// var pattern = /man/gim;
-// var myarray = str.match( pattern );
-// var len = myarray.length;
-// console.log(`Occurance :${len}`);
+console.log("Occurrences of pattern in the string is : " + len);
