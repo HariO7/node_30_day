ffe36dc53f02d35bbe084c092d2ca221fc703a65
hari <hariodukkathil@gmail.com>
2022-01-27 13:15:07 +0530

    password validation

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let str = "00000A";
-let pattern = /[A-Fa-f0-9]+$/g;
+let str = "Aa#";
+let pattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,16}$/g;
 
 let res = str.match(pattern);
 if(res){
-	console.log("It is a hexadecimal");
+	console.log("It is a valild password");
 }else{
-	console.log("Not a hexadecimal");
+	console.log("it is a invalid password");
 }
