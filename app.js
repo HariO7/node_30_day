c3712b64a5383e5914073533cf4d846b6bd23c53
hari <hariodukkathil@gmail.com>
2022-02-01 13:16:19 +0530

    foreach

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 arr =[2,4,6,8,10];
 arr2 =[3,5,7,9,22];
 
-function even(value){
-    if( (value%2) == 0){
-      return true;
-    }else{
-
-      return false;
-    }
-}
-
-var out = arr.filter(even);
-var out2 = arr2.filter(even);
-
-console.log(out);
-console.log(out2);
-
+arr.forEach(e => {
+  console.log(e);
+  
+});
 
