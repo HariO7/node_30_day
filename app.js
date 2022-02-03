7aa6c9cf691761b5a0daa3131001b76fecd29eef
hari07 <hariodukkathil@gmail.com>
2022-02-03 11:19:19 +0530

    hmac encryption

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-//Loading the crypto module in node.js
+// Including the required modules
 var crypto = require('crypto');
-//creating hash object 
-var hash = crypto.createHash('whirlpool');
+var fs = require('fs');
+
+//Algorithm to be used for HMAC
+var algorithm = 'md5';
+//Secret to be used with HMAC
+var secret ='Rj2895647';
+//creating hmac object
+var hmac = crypto.createHmac(algorithm, secret);
+
+// reading the content of the file
+var filename = "data.txt";
+var file_data = fs.ReadStream(filename);
+
 //passing the data to be hashed
-data = hash.update('nodejsera', 'utf-8');
-//Creating the hash in the required format
-gen_hash= data.digest('hex');
-//Printing the output on the console
-console.log("hash : " + gen_hash);
+file_data.on('data', function(data) {
+  hmac.update(data)
+})
+
+//Creating the hmac in the required format and writing it in file
+file_data.on('end', function() {
+  var gen_hmac = hmac.digest('hex')
+  console.log('Hmac generated using ' + algorithm + ' \nHashed output is :  ' + gen_hmac + ' \nFile name is :  ' + filename);
+  fs.writeFileSync(filename, gen_hmac);
+})
