f7342ca6d02e0a99cd046738a4514f120d2691e3
hari07 <hariodukkathil@gmail.com>
2022-02-25 16:04:12 +0530

    stringify

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const querystring = require('querystring');
 
-let value_json = querystring.parse('id=1&name=rj&name=njsera&tutorial=30daysofnode&creator=nodejsera')
+let value_json = querystring.stringify({ id: 1, name: ['abc', 'njera'], tutorial: '30days of node', creator : 'nodejsera' })
 console.log(value_json);
