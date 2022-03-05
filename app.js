0bfa37167fc3a930349ac2c3d9b5af5add7e56a9
hari07 <hariodukkathil@gmail.com>
2022-03-05 11:34:55 +0530

    cluster

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-var cp = require('child_process');
- 
-for(var i = 1; i<6; i++) {
-   var worker = cp.spawn('node', ['slave.js', i]);
+const cluster = require('cluster');
+const http = require('http');
+const numofCPUs = require('os').cpus().length;
 
-   worker.stdout.on('data', function (data) {
-      console.log('Value of Stdout : ' + data);
-   });
+if(cluster.isMaster){
+   console.log(`master Process ID : ${process.pid} is running` );
 
-   worker.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
+   
+   for(let i; i< numofCPUs;i++){
+      cluster.fork();
+   }
+   
+   cluster.on('exit',(worker,code,signal)=>{
+      console.log(`worker with Process ID : ${worker.process.pid} died`);
    });
+}else{ 
+   // Workers sharing an HTTP server
+  http.createServer((req, res) => {
+   res.writeHead(200);
+   res.end('An example of clusters\n');
+ }).listen(3000);
+ console.log(`Worker with Process ID : ${process.pid} started`);
 
-   worker.on('close', function (code) {
-       //console.log("Exit code : " + code);
-      console.log('child process exited with code ');
-   });
 }
