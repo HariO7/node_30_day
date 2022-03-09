99ad1f1e86b2da63ac5a30abd8744e4234876cca
hari07 <hariodukkathil@gmail.com>
2022-03-09 12:31:46 +0530

    assert1

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const os = require('os');
-
-const value = os.type()
-console.log('os.cpus() ===> ' + value);
+var assert = require('assert');  
+function demo (x,y,z) {  
+  var value = x + y + z ; 
+  return value;
+}  
+var output = demo(4,1,10);
+console.log("Output : " + output);
+var expected_output = 12;
+console.log("Expected Output : " + expected_output);
+assert( output === expected_output , 'Test case is true so this will not be printed');
