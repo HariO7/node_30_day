0d878eac0be66e9b26a8ddcbbb9f77713597942f
hari07 <hariodukkathil@gmail.com>
2022-03-09 12:25:21 +0530

    OS

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-const cluster = require('cluster');
-const http = require('http');
-const numofCPUs = require('os').cpus().length;
+const os = require('os');
 
-if(cluster.isMaster){
-   console.log(`master Process ID : ${process.pid} is running` );
-
-   
-   for(let i; i< numofCPUs;i++){
-      cluster.fork();
-   }
-   
-   cluster.on('exit',(worker,code,signal)=>{
-      console.log(`worker with Process ID : ${worker.process.pid} died`);
-   });
-}else{ 
-   // Workers sharing an HTTP server
-  http.createServer((req, res) => {
-   res.writeHead(200);
-   res.end('An example of clusters\n');
- }).listen(3000);
- console.log(`Worker with Process ID : ${process.pid} started`);
-
-}
+const value = os.type()
+console.log('os.cpus() ===> ' + value);
